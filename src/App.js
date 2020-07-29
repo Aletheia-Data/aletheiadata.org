33028a35a8a7de8198f52d3cac5d38d8cc4aad4d
Enzo Vezzaro <enzo-vezzaro@live.it>
2020-07-29 07:16:31 +0200

    remove test upload

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import {
 // import pages
 import Demo from './pages/demo/demo.jsx';
 import NotFound from './pages/404-not-found/4040-not-found.jsx';
-import Upload from './pages/upload/Upload.jsx';
 
 export default function App() {
   return (
@@ -17,9 +16,6 @@ export default function App() {
         <Switch>
           <Route path="/demo">
             <Demo />
-          </Route>
-          <Route path="/upload">
-            <Upload />
           </Route>
           <Route path="*">
             <NotFound />
--- src/pages/404-not-found/4040-not-found.jsx
+++ src/pages/404-not-found/4040-not-found.jsx
@@ -3,6 +3,6 @@ import React from "react";
 export default function NotFound() {
     window.location.replace('https://docs.aletheiadata.org')
   return (
-    <div>Not found</div>
+    <div></div>
   );
 }
